77b20590fe795e8f1f491426152a4b2499c7b0cd
Alex Rock Ancelet <pierstoval@gmail.com>
2024-10-23 11:34:30 +0200

    Fix cs and e2e

---
+++ backend/.php-cs-fixer.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+;
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+    ])
+    ->setFinder($finder)
+;
