784e746ed345b24478028d88018ff536a5e43f85
Alex Rock Ancelet <pierstoval@gmail.com>
2024-12-02 14:26:52 +0100

    Update deps, now EA needs twig components, yay

--- backend/config/bundles.php
+++ backend/config/bundles.php
@@ -14,4 +14,5 @@ return [
     Pierstoval\SmokeTesting\SmokeTestingBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\UX\TwigComponent\TwigComponentBundle::class => ['all' => true],
 ];
